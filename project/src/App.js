018d519d4f8ec2c4f05325e93e9dbdb1c11d3680
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-12-17 18:24:42 +0000

    configure application and create structureof project

--- project/src/App.js
+++ project/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+import Header from './components/Header';
+import GroupsTable from './components/GroupsTable';
+import StudentsTable from './components/StudentsTable';
+import CouncilTable from './components/CouncilTable';
+import TeachersTable from './components/TeachersTable';
+import EventsListTable from './components/EventsListTable';
+import EventsLogTable from './components/EventsLogTable';
+import ReportTable from './components/ReportTable';
+import Redirect from './components/Redirect';
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <Header />
+          <Routes>
+              <Route path="/" element={<Redirect url="/groups" />} />
+              <Route path="/groups" element={<GroupsTable />} />
+              <Route path="/students" element={<StudentsTable />} />
+              <Route path="/student-council" element={<CouncilTable />} />
+              <Route path="/teachers" element={<TeachersTable />} />
+              <Route path="/events-list" element={<EventsListTable />} /> 
+              <Route path="/events-log" element={<EventsLogTable />} />
+              <Route path="/report" element={<ReportTable />} />
+          </Routes>
+      </BrowserRouter>
   );
 }
-
-export default App;
