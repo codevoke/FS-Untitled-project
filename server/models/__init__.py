49da8cf145e1dd7cb72db9ddbb86c0a3f4c50826
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-12-18 03:31:13 +0000

    update module imports

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,3 +1,12 @@
 from .db import db
 
-# from .model import ModelClass
+from .Calendar import Calendar
+from .DivisionList import DivisionList
+from .EventList import EventList
+from .EventType import EventType
+from .GroupList import GroupList
+from .Involvement import Involvement
+from .StaffList import StaffList
+from .Student import Student
+from .StudentBoard import StudentBoard
+from .StudyType import StudyType
